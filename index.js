bb37f359a5dc7f436f11b767a019e0e4e801ca37
Shreedhar Hegde <snhkelemane@gmail.com>
2021-12-08 10:10:17 +0530

    minor update

--- index.js
+++ index.js
@@ -5,35 +5,58 @@ btn.addEventListener("click", addToTask);
 
 window.addEventListener("load", renderDOM);
 
+
+//display all the tasks
 function renderDOM() {
-    let count   =1;
+  let count = 1;
   let storage = window.localStorage;
-  let items = JSON.parse(storage.getItem("tasks"));
+  let items = JSON.parse(storage.getItem("tasks")) || [];
   let div = document.querySelector(".tasks");
   for (let i = 0; i < items.length; i++) {
     let p = document.createElement("p");
-    p.textContent = `${count++}.${items[i]}`;
+    p.textContent = `${count++}.${items[i].task}`;
     div.append(p);
   }
 }
 
 
-
-
+// adding tasks to the list
 function addToTask() {
   let p = document.createElement("p");
   if (task.value != "") {
     let storage = window.localStorage;
     let items = JSON.parse(storage.getItem("tasks")) || [];
-    items.push(task.value);
+    // console.log(items)
+    class Task {
+      constructor(task, status = false) {
+        this.task = task;
+        this.status = status;
+      }
+    }
+
+    let tasks = new Task(task.value);
+
+    items.push(tasks);
+    // console.log(items)
     storage.setItem("tasks", JSON.stringify(items));
-    console.log(items);
-    let div =   document.querySelector(".tasks")
+
+    let div = document.querySelector(".tasks");
     div.textContent = "";
-    
-    renderDOM()
+
+    renderDOM();
     task.value = "";
   } else {
-    alert("Please give input to add");
+    alert("Please give any task to add");
   }
 }
+
+
+let dltbtn  = document.querySelector(".deletebtn")
+dltbtn.addEventListener("click",deleteAll)
+
+function  deleteAll(){
+  window.localStorage.removeItem("tasks")
+  document.querySelector(".tasks").textContent  = ""
+}
+
+
